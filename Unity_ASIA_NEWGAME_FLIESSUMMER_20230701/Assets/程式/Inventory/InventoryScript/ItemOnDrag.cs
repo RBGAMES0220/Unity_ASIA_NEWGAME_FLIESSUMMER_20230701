20797b8c9ba1136255afd0f9ba1e56940d52a2c0
justin28668221 <96966547+fat28668221@users.noreply.github.com>
2023-09-13 13:14:15 +0800

    2023913

---
+++ Unity_ASIA_NEWGAME_FLIESSUMMER_20230701/Assets/程式/Inventory/InventoryScript/ItemOnDrag.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemOnDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform originaParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originaParent = transform.parent;
+        transform.SetParent(transform.parent. parent);
+        transform.position = eventData.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(eventData.pointerCurrentRaycast.gameObject.name == "item Image")
+        {
+            transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
+            transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.parent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.position = originaParent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(originaParent);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            return;
+        }
+        transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
+        transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+  
+}
+
+
---
+++ Unity_ASIA_NEWGAME_FLIESSUMMER_20230701/Assets/程式/Inventory/InventoryScript/MoveBag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveBag : MonoBehaviour,IDragHandler
+{
+    public Canvas canvas;
+    RectTransform currenRect;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currenRect.anchoredPosition += eventData.delta;
+    }
+
+    void Awake()
+    {
+        currenRect = GetComponent<RectTransform>();
+    }
+}
