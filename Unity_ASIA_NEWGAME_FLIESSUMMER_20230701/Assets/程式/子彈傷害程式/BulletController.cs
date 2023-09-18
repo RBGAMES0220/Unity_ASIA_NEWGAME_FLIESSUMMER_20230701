e8dae3c7a26b0193f51c28cd504a23499b5dd50b
RBGAMES0220 <129024364+RBGAMES0220@users.noreply.github.com>
2023-09-18 11:08:26 +0800

    更新穿牆bug

---
+++ Unity_ASIA_NEWGAME_FLIESSUMMER_20230701/Assets/程式/子彈傷害程式/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GLORY
+{
+    public class BulletController : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            // 檢查碰撞是否是牆壁（你可以根據你的遊戲設計使用不同的判斷方法）
+            if (collision.gameObject.CompareTag("Map"))
+            {
+                // 如果碰撞到牆壁，銷毀子彈物體
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
