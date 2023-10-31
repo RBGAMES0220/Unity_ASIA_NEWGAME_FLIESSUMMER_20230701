d379e397851ff477d350a40d59dd44e401002658
RBGAMES0220 <129024364+RBGAMES0220@users.noreply.github.com>
2023-11-01 01:00:43 +0800

    遊戲主畫面

---
+++ Unity_ASIA_NEWGAME_FLIESSUMMER_20230701/Assets/遊戲開始畫面/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace GLORY
+{
+    public class MenuManager : MonoBehaviour
+    {
+        private Button btnPlay;
+
+        private void Awake()
+        {
+            btnPlay = GameObject.Find("開始遊戲").GetComponent<Button>();
+            btnPlay.onClick.AddListener(StartGame);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene("01_FLIES_森林 _new");
+        }
+    }
+}
+
