798cdebf5bf0c73e8fa7bd3d93c2cb95ad61b0b9
RBGAMES0220 <129024364+RBGAMES0220@users.noreply.github.com>
2023-11-01 23:58:45 +0800

    打包測試

---
+++ Unity_ASIA_NEWGAME_FLIESSUMMER_20230701/Assets/Scripts/玩家 敵人 存檔 程式/Mood.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Mood : MonoBehaviour
+{
+    [SerializeField] private GameObject _mood;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _mood?.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _mood?.SetActive(false);
+        }
+    }
+}
